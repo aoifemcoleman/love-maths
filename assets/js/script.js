246ead09b7f9cf591a34a44117359c6a916382e8
Aoife Coleman <aoifemcoleman@hotmail.com>
2023-12-30 17:36:47 +0000

    Add favicon and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    }) 
     runGame("addition");
 
 });
@@ -22,6 +28,10 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 function runGame(gameType) {
+    // empties answer box after submission
+    document.getElementById("answer-box").value = "";
+    // inserts cursor in box when runGame function is called
+    document.getElementById("answer-box").focus();
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
